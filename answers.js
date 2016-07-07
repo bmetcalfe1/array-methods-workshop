d4ab0678c3981eceae21d10eb8316414f4c48604
Brendan Metcalfe <bgmetcalfe@gmail.com>
2016-07-07 18:19:11 +0000

    updated second q

--- answers.js
+++ answers.js
@@ -16,13 +16,33 @@
 //write a function called getPositives that takes an array and uses the filter method to return a new array with only the positive numbers.
 
 var theArray = [1, -1, 3, -3, 4, -4];
+var newArray = [];
+
 
 function getPositives (numArray) {
-    numArray.filter(function(num){
-        if (num >= 0){
-            console.log(num);
-        }
-    });
-}
+    return numArray.filter(function(num){
+         if (num >= 0){
+             return num;
+         }
+     });
+ }
 
-getPositives(theArray);
+ var makePos = getPositives(theArray);
+ newArray.push(makePos);
+ 
+ console.log(newArray);
+
+//Re-do exercise 1 by first filtering the input array, and then printing the numbers from the filtered array. 
+// Your code will look something like: return arr.filter(...).forEach(...).
+
+// var theArray = [1, 2, -3, 4, -5];
+
+// function printPositives (numArray) {
+//      numArray.filter(function(num){
+//          if (num >= 0){
+//              console.log(num);
+//          }
+//      });
+//  }
+
+//  printPositives(theArray);
