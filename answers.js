53fe958c3f228a8f931f807a8a5afce88be4d70b
Brendan Metcalfe <bgmetcalfe@gmail.com>
2016-07-07 21:38:16 +0000

    completed 6th q

--- answers.js
+++ answers.js
@@ -105,15 +105,53 @@
 
 // countVowels("I am the greatest rapper alive");
 
-function countVowels (anyString) {
-    var strSplit = anyString.split('');
+// function countVowels (anyString) {
+//     var strSplit = anyString.split('');
 
-    return strSplit.reduce(function(totalVowels, isVowel) {
-        if (isVowel === "a" || isVowel === "e" || isVowel === "i" || isVowel === "o" || isVowel === "u") {
-            totalVowels += 1;
+//     return strSplit.reduce(function(totalVowels, isVowel) {
+//         if (isVowel === "a" || isVowel === "e" || isVowel === "i" || isVowel === "o" || isVowel === "u") {
+//             totalVowels += 1;
+//         }
+//         return totalVowels;
+//     }, 0);
+// }
+
+// console.log(countVowels("I am the greatest rapper alive"));
+
+//EXERCISE 6
+
+// Write a function called highLow that takes an array of numbers, 
+// and returns an object with a property highest containing the highest number, 
+// and a property lowest containing the lowest number, using Array.prototype.reduce.
+
+//For example, starting with [1, -10, 20, 40, 5], your function should return {highest: 40, lowest: -10}.
+// Hint: Javascript has a special value called Infinity, which is higher than any other number. 
+//See if you can initialize your reduce accumulator with Infinity and -Infinity :)
+
+function highLow (numArray) {
+    var getsHighest = numArray.reduce(function(highestNumber, currentNumber) {
+        if (currentNumber > highestNumber) {
+            return currentNumber;
         }
-        return totalVowels;
-    }, 0);
+        else {
+            return highestNumber;
+        }
+    });
+    
+    var getsLowest = numArray.reduce(function(lowestNumber, currentNumber) {
+        if (currentNumber < lowestNumber) {
+            return currentNumber;
+        }
+        else {
+            return lowestNumber;
+        }
+    });
+    
+    var result = {
+    highest: getsHighest,
+    lowest: getsLowest
+    };
+    console.log(result);
 }
 
-console.log(countVowels("I am the greatest rapper alive"));
+highLow([2, 342234, 6, -5, 8, 11]);
